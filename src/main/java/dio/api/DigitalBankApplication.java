e1c0bef1992ace6114900b26c9bbdfb4867e5474
Danilo Samways <hadeflowing@gmail.com>
2024-06-01 22:14:35 -0300

    refactor: change application name

--- src/main/java/dio/api/DigitalBankApplication.java
+++ src/main/java/dio/api/DigitalBankApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrimeirosPassosApplication {
+public class DigitalBankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PrimeirosPassosApplication.class, args);
+		SpringApplication.run(DigitalBankApplication.class, args);
 	}
 
 }
